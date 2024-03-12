9f59db8ff52528b54d55ded681deed6250814912
Ben Cavins <ben.cavins@flatironschool.com>
2024-03-12 16:30:21 -0400

    do part of lab

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -1,4 +1,4 @@
-spicy_foods = [
+foods = [
     {
         "name": "Green Curry",
         "cuisine": "Thai",
@@ -23,7 +23,11 @@ def get_spiciest_foods(spicy_foods):
     pass
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for food_dict in spicy_foods:
+        peppers = food_dict['heat_level'] * '🌶'
+        print(f"{food_dict['name']} ({food_dict['cuisine']}) | Heat Level: {peppers}")
+
+print_spicy_foods(foods)
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
     pass
@@ -32,7 +36,14 @@ def print_spiciest_foods(spicy_foods):
     pass
 
 def get_average_heat_level(spicy_foods):
-    pass
+    total = 0
+    for food_dict in spicy_foods:
+        total += food_dict['heat_level']
+    return total / len(spicy_foods)
+
+get_average_heat_level(foods)
 
 def create_spicy_food(spicy_foods, spicy_food):
     pass
+
+
